a8061ff63f255421ac5afe33fc454fc52a1c92e6
williams <ikswaqqs40@gmail.com>
2017-01-19 02:12:13 +0300

    1_commit

---
+++ Assets/_scripts/Game_manager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_manager : MonoBehaviour {
+
+	public static Game_manager instance = null;
+	public GameObject spawnPoint;
+	public GameObject[] enemies;
+	public int maxEnemiesOnScreen;
+	public int totalEnemies;
+	public int enemiesPerSpawn;
+
+	private int enemiesOnscreen = 0;
+	const float spawn_delay = 0.5f;
+	
+	void Awake() {
+		if (instance == null) 
+			instance = this;
+		
+		 else if (instance != this)
+			Destroy (gameObject);
+		
+		DontDestroyOnLoad (gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		//spawn_enemy ();	
+		StartCoroutine 	(spawn());
+	}
+	
+	/*void spawn_enemy() {
+		if (enemiesPerSpawn > 0 && enemiesOnscreen < totalEnemies) {
+			for (int i = 0; i < enemiesPerSpawn; i++) {
+				if (enemiesOnscreen < maxEnemiesOnScreen) {
+					GameObject newEnemy = Instantiate (enemies [0] as GameObject);
+					newEnemy.transform.position = spawnPoint.transform.position;
+					enemiesOnscreen += 1;
+				
+				}
+			
+			}
+		}
+	
+	}*/
+
+	IEnumerator spawn() {
+		if (enemiesPerSpawn > 0 && enemiesOnscreen < totalEnemies) {
+			for (int i = 0; i < enemiesPerSpawn; i++) {
+				if (enemiesOnscreen < maxEnemiesOnScreen) {
+					GameObject newEnemy = Instantiate (enemies [0] as GameObject);
+					newEnemy.transform.position = spawnPoint.transform.position;
+					enemiesOnscreen += 1;
+
+				}
+
+			}
+			yield return new WaitForSeconds (spawn_delay);
+			StartCoroutine (spawn ());
+		}
+	
+	}
+
+	public void removeEnemyFromScreen() {
+		if (enemiesOnscreen > 0) { 
+			enemiesOnscreen -= 1;
+	
+		}
+	}
+}
---
+++ Assets/_scripts/Loader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+	public GameObject game_manager;
+	void Awake() {
+		if (Game_manager.instance == null) 
+			Instantiate (game_manager);
+		
+		/*KeyValuePair<int,string> course = new KeyValuePair<int, string>(1,"fuck society");
+		course.printKeyValue ();
+
+		KeyValuePair<string,string> lesson = new KeyValuePair<string, string>("society 101","death to society");
+		lesson.printKeyValue ();*/
+
+
+	}
+
+}
+	/*public class KeyValuePair<Tkey,Tvalue> {
+
+		public Tkey key;
+		public Tvalue value;
+
+		public KeyValuePair(Tkey key,Tvalue value) {
+
+			this.key = key;
+			this.value = value;
+
+		}
+
+		public void printKeyValue(){
+
+			Debug.Log ("key " + key.ToString());
+			Debug.Log ("value " + value.ToString());
+		
+		}
+
+}*/
